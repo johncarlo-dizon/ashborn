0a1cb3acdb3272b9485b31787e4825a0c17769d2
johncarlo-dizon <johncarlovdizon@gmail.com>
2025-03-03 20:03:22 +0800

    quote category

---
+++ lib/quote_category.dart
@@ -0,0 +1,306 @@
+import 'package:flutter/cupertino.dart';
+import 'package:ashborn/category.dart';
+import 'package:ashborn/confidence.dart';
+import 'package:ashborn/inspirational.dart';
+import 'package:ashborn/love.dart';
+import 'package:ashborn/motivational.dart';
+import 'package:ashborn/philosopical.dart';
+import 'package:ashborn/positive.dart';
+import 'package:ashborn/sad.dart';
+
+class CategoryQuote extends StatefulWidget {
+  const CategoryQuote({super.key});
+
+  @override
+  State<CategoryQuote> createState() => _CategoryQuoteState();
+}
+
+class _CategoryQuoteState extends State<CategoryQuote> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          leading: CupertinoButton(
+              padding: EdgeInsets.zero,
+              child: Icon(
+                CupertinoIcons.left_chevron,
+                size: 20,
+                color: Color(0xFF1A120B),
+              ),
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                    context,
+                    CupertinoPageRoute(builder: (context) => CategoryApp()),
+                    (Route<dynamic> route) => false);
+              }),
+          middle: Text(
+            'Quotes',
+            style: TextStyle(color: Color(0xFF1A120B)),
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: SafeArea(
+                child: Column(
+                  children: [
+                    //MOTIVATION START
+                    Container(
+                      width: double.maxFinite,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFd9d9d9),
+                        borderRadius: BorderRadius.circular(10),
+                        image: MediaQuery.of(context).size.width > 320
+                            ? DecorationImage(
+                          image: AssetImage('images/self-confidence.png'),
+                          alignment: Alignment.centerRight,
+                          fit: BoxFit.contain,
+                        )
+                            : null,
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
+                        child: CupertinoButton(
+                            child: Text(
+                              'Motivational Quotes',
+                              style: TextStyle(color:  Color(0xFF262626)),
+                            ),
+                            alignment: Alignment.topLeft,
+                            onPressed: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  CupertinoPageRoute(
+                                      builder: (context) => Motivational()));
+                            }),
+                      ),
+                    ),
+
+                    SizedBox(
+                      height: 10,
+                    ),
+                    //MOTIVATION END
+
+                    //MOTIVATION START
+                    Container(
+                      width: double.maxFinite,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFf2f2f2),
+                        borderRadius: BorderRadius.circular(10),
+                        image: MediaQuery.of(context).size.width > 320
+                            ? DecorationImage(
+                          image: AssetImage('images/love-books.png'),
+                          alignment: Alignment.centerRight,
+                          fit: BoxFit.contain,
+                        )
+                            : null,
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
+                        child: CupertinoButton(
+                            child: Text(
+                              'Love Quotes',
+                              style: TextStyle(color:  Color(0xFF0d0d0d)),
+                            ),
+                            alignment: Alignment.topLeft,
+                            onPressed: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  CupertinoPageRoute(
+                                      builder: (context) => LoveApp()));
+                            }),
+                      ),
+                    ),
+
+                    SizedBox(
+                      height: 10,
+                    ),
+                    //MOTIVATION END
+
+                    //MOTIVATION START
+                    Container(
+                      width: double.maxFinite,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFd9d9d9),
+                        borderRadius: BorderRadius.circular(10),
+                        image: MediaQuery.of(context).size.width > 320
+                            ? DecorationImage(
+                          image: AssetImage('images/inspiration.png'),
+                          alignment: Alignment.centerRight,
+                          fit: BoxFit.contain,
+                        )
+                            : null,
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
+                        child: CupertinoButton(
+                            child: Text(
+                              'Inspirational Quotes',
+                              style: TextStyle(color:  Color(0xFF262626)),
+                            ),
+                            alignment: Alignment.topLeft,
+                            onPressed: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  CupertinoPageRoute(
+                                      builder: (context) => InspiApp()));
+                            }),
+                      ),
+                    ),
+
+                    SizedBox(
+                      height: 10,
+                    ),
+                    //MOTIVATION END
+
+                    //MOTIVATION START
+                    Container(
+                      width: double.maxFinite,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFf2f2f2),
+                        borderRadius: BorderRadius.circular(10),
+                        image: MediaQuery.of(context).size.width > 320
+                            ? DecorationImage(
+                          image: AssetImage('images/thumbs-up.png'),
+                          alignment: Alignment.centerRight,
+                          fit: BoxFit.contain,
+                        )
+                            : null,
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
+                        child: CupertinoButton(
+                            child: Text(
+                              'Positive Quotes',
+                              style: TextStyle(color: Color(0xFF0d0d0d)),
+                            ),
+                            alignment: Alignment.topLeft,
+                            onPressed: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  CupertinoPageRoute(
+                                      builder: (context) => Positive()));
+                            }),
+                      ),
+                    ),
+
+                    SizedBox(
+                      height: 10,
+                    ),
+                    //MOTIVATION END
+
+                    //MOTIVATION START
+                    Container(
+                      width: double.maxFinite,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFd9d9d9),
+                        borderRadius: BorderRadius.circular(10),
+                        image: MediaQuery.of(context).size.width > 320
+                            ? DecorationImage(
+                          image: AssetImage('images/sad1.png'),
+                          alignment: Alignment.centerRight,
+                          fit: BoxFit.contain,
+                        )
+                            : null,
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
+                        child: CupertinoButton(
+                            child: Text(
+                              'Sad Quotes',
+                              style: TextStyle(color:  Color(0xFF262626)),
+                            ),
+                            alignment: Alignment.topLeft,
+                            onPressed: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  CupertinoPageRoute(
+                                      builder: (context) => Sad()));
+                            }),
+                      ),
+                    ),
+
+                    SizedBox(
+                      height: 10,
+                    ),
+                    //MOTIVATION END
+
+                    //MOTIVATION START
+                    Container(
+                      width: double.maxFinite,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFf2f2f2),
+                        borderRadius: BorderRadius.circular(10),
+                        image: MediaQuery.of(context).size.width > 320
+                            ? DecorationImage(
+                          image: AssetImage('images/empire.png'),
+                          alignment: Alignment.centerRight,
+                          fit: BoxFit.contain,
+                        )
+                            : null,
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
+                        child: CupertinoButton(
+                            child: Text(
+                              'Philosophical Quotes',
+                              style: TextStyle(color:  Color(0xFF0d0d0d)),
+                            ),
+                            alignment: Alignment.topLeft,
+                            onPressed: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  CupertinoPageRoute(
+                                      builder: (context) => Philosopical()));
+                            }),
+                      ),
+                    ),
+
+                    SizedBox(
+                      height: 10,
+                    ),
+                    //MOTIVATION END
+
+                    //MOTIVATION START
+                    Container(
+                      width: double.maxFinite,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFd9d9d9),
+                        borderRadius: BorderRadius.circular(10),
+                        image: MediaQuery.of(context).size.width > 320
+                            ? DecorationImage(
+                          image: AssetImage('images/confidence.png'),
+                          alignment: Alignment.centerRight,
+                          fit: BoxFit.contain,
+                        )
+                            : null,
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
+                        child: CupertinoButton(
+                            child: Text(
+                              'Confident Quotes',
+                              style: TextStyle(color: Color(0xFF262626)),
+                            ),
+                            alignment: Alignment.topLeft,
+                            onPressed: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  CupertinoPageRoute(
+                                      builder: (context) => Confident()));
+                            }),
+                      ),
+                    ),
+
+                    SizedBox(
+                      height: 10,
+                    ),
+                    //MOTIVATION END
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ));
+  }
+}
